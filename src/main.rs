75ef11a9b3cae3622748f260bf534c92afb616b2
Murilo Cruz <murilomecr@gmail.com>
2026-09-25 13:13:26 -0300

    adding layer to the app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::time::Duration;
+
 use axum::{Json, Router, http::StatusCode, routing::get};
 
 use serde::{Deserialize, Serialize};
+use tower::ServiceBuilder;
+use tower_http::{
+    ServiceBuilderExt, compression::CompressionLayer, cors::CorsLayer,
+    limit::RequestBodyLimitLayer, request_id::MakeRequestUuid, timeout::TimeoutLayer,
+    trace::TraceLayer,
+};
 
 #[derive(Clone)]
 struct AppState {
@@ -48,6 +56,10 @@ async fn health() -> Json<HealthResponse> {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter("info,tower_http=debug")
+        .init();
+
     let config: Config = match Config::from_env() {
         Ok(loaded) => loaded,
         Err(err) => {
@@ -62,7 +74,20 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root))
         .route("/health", get(health))
-        .with_state(state);
+        .with_state(state)
+        .layer(
+            ServiceBuilder::new()
+                .set_x_request_id(MakeRequestUuid::default())
+                .propagate_x_request_id()
+                .layer(TraceLayer::new_for_http())
+                .layer(CorsLayer::permissive()) // troque por política real
+                .layer(TimeoutLayer::with_status_code(
+                    StatusCode::REQUEST_TIMEOUT,
+                    Duration::from_secs(10),
+                ))
+                .layer(RequestBodyLimitLayer::new(1024 * 1024)) // 1 MiB
+                .layer(CompressionLayer::new()),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
